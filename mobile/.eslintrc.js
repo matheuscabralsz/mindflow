70fb8add831152a64caab894450a82e5a4a51d64
Matheus Cabral <skt.matheus@hotmail.com>
2025-11-09 12:18:17 -0300

    Add ESLint and TypeScript configurations for mobile app development
    - Configure `.eslintrc.js` with recommended rules and plugins for React, React Native, and TypeScript.
    - Update `tsconfig.json` to enforce strict typing, improve code quality, and enable module resolution with path aliases.

---
+++ mobile/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  extends: [
+    'expo',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-native'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  env: {
+    'react-native/react-native': true,
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
